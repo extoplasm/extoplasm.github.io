c53e3119f253b6a5b2af73aa3d5e5cc74f3b2481
extoplasm <pleassehelsp@gmail.com>
2024-10-24 22:01:17 +1100

    i think im about done

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { Canvas } from '@react-three/fiber';
+import { useState } from 'react';
 import Box from './assets/Box.tsx'; 
 import './App.css';
 
+function copyToClipboard(text: string) {
+	navigator.clipboard.writeText(text)
+        .catch(err => {
+            console.error('Failed to copy: ', err);
+        });
+}
+
 export default function App() {
+	const [copiedDiscord, setCopiedDiscord] = useState(false)
 	return (
 		<>
 			<div className="text-container left">
@@ -17,8 +26,18 @@ export default function App() {
 			<div className="text-container middle">
 				<h3>links</h3>
 				<a className="link" href="https://www.youtube.com/@extoplasm" target="_blank" rel="noopener noreferrer">youtube</a>
+				<br/>
+				<a className="link" href="http://github.com/extoplasm" target="_blank" rel="noopener noreferrer">github</a>
+				<br/>
+				<button className="link" onClick={() => {
+					copyToClipboard('@extoplasm')
+					setCopiedDiscord(true)
+					setTimeout(() => {
+						setCopiedDiscord(false)
+					}, 1000)
+				}}>{copiedDiscord ? 'copied to clipboard!': 'discord' }</button>
 			</div>
-			<Canvas>
+			<Canvas className="canvas">
 				<ambientLight intensity={Math.PI / 2} />
 				<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={1} />
 				<Box position={[0, 0, 0]} />
