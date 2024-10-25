ed9fa2d8cba2e18c8815bcec810609e75f90990f
extoplasm <pleassehelsp@gmail.com>
2024-10-25 18:23:00 +1100

    add(model): added model removed box changedup layo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { Canvas } from '@react-three/fiber';
 import { useState } from 'react';
-import Box from './assets/Box.tsx'; 
+import Ghost from './assets/Ghost.tsx';
 import './App.css';
 
 function copyToClipboard(text: string) {
@@ -10,21 +10,24 @@ function copyToClipboard(text: string) {
         });
 }
 
+function Bio() {
+	return (
+		<>
+			<h1 className="margin-10">extoplasm ©</h1>
+			<h2 className="margin-10">i code (sometimes)</h2>
+		</>
+	)
+}
+
 export default function App() {
 	const [copiedDiscord, setCopiedDiscord] = useState(false)
 	return (
 		<>
 			<main className="content">
-				<div className="text-container left">
-					<h1 className="margin-10">extoplasm (c)</h1>
-					<h2 className="margin-10">i code (sometimes)</h2>
+				<div className="text-container">
+					<Bio/>
 				</div>
-				<div className="text-container right">
-					<h1 className="margin-10">extoplasm ©</h1>
-					<h2 className="margin-10">i code (sometimes)</h2>
-				</div>
-
-				<div className="text-container middle">
+				<div className="text-container margin-10">
 					<h3>links</h3>
 					<a className="link" href="https://www.youtube.com/@extoplasm" target="_blank" rel="noopener noreferrer">youtube</a>
 					<br/>
@@ -40,9 +43,9 @@ export default function App() {
 				</div>
 			</main>
 			<Canvas className="canvas">
-				<ambientLight intensity={Math.PI / 2} />
+				<ambientLight intensity={0.1} />
 				<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={1} />
-				<Box position={[0, 0, 0]} />
+				<Ghost position={[0, -1, 0]} />
 			</Canvas>
 		</>
 	)
---
+++ src/assets/Ghost.tsx
@@ -0,0 +1,39 @@
+import * as THREE from 'three';
+import { useFrame, ThreeElements } from '@react-three/fiber';
+import { useGLTF } from '@react-three/drei';
+import { useEffect, useRef, useState } from 'react';
+import { GLTF } from 'three/examples/jsm/Addons.js';
+
+const GhostGeometry = () => {
+	const { scene } = useGLTF('../../ghost.glb') as unknown as GLTF;
+
+    return <primitive object={scene} />;
+}
+
+export default function Ghost(props: ThreeElements['mesh']) {
+	const meshRef = useRef<THREE.Mesh>(null!)
+
+	const [hovered, setHover] = useState(false);
+    
+    // offset rotation
+    useEffect(() => {
+        meshRef.current.rotation.y = Math.floor(Math.random() * Math.PI)
+    }, [])
+
+	// rotate
+	useFrame((_state, delta) => {
+		meshRef.current.rotation.y -= (hovered ? delta*4 : delta/2)
+	});
+
+	return (
+		<mesh
+			{...props}
+			ref = {meshRef}
+			onPointerOver = {(_e) => setHover(true)}
+			onPointerOut = {(_e) => setHover(false)}
+		>
+			<GhostGeometry/>
+		</mesh>
+	)
+}
+
