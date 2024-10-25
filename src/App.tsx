91fa12e3d2aec5f75e799fce4219a2d3d0704f33
extoplasm <pleassehelsp@gmail.com>
2024-10-25 17:36:24 +1100

    fix: pointerevents wasnt workign on canvas

--- src/App.tsx
+++ src/App.tsx
@@ -14,29 +14,31 @@ export default function App() {
 	const [copiedDiscord, setCopiedDiscord] = useState(false)
 	return (
 		<>
-			<div className="text-container left">
-				<h1 className="margin-10">extoplasm (c)</h1>
-				<h2 className="margin-10">i code (sometimes)</h2>
-			</div>
-			<div className="text-container right">
-				<h1 className="margin-10">extoplasm ©</h1>
-				<h2 className="margin-10">i code (sometimes)</h2>
-			</div>
+			<main className="content">
+				<div className="text-container left">
+					<h1 className="margin-10">extoplasm (c)</h1>
+					<h2 className="margin-10">i code (sometimes)</h2>
+				</div>
+				<div className="text-container right">
+					<h1 className="margin-10">extoplasm ©</h1>
+					<h2 className="margin-10">i code (sometimes)</h2>
+				</div>
 
-			<div className="text-container middle">
-				<h3>links</h3>
-				<a className="link" href="https://www.youtube.com/@extoplasm" target="_blank" rel="noopener noreferrer">youtube</a>
-				<br/>
-				<a className="link" href="http://github.com/extoplasm" target="_blank" rel="noopener noreferrer">github</a>
-				<br/>
-				<button className="link" onClick={() => {
-					copyToClipboard('@extoplasm')
-					setCopiedDiscord(true)
-					setTimeout(() => {
-						setCopiedDiscord(false)
-					}, 1000)
-				}}>{copiedDiscord ? 'copied to clipboard!': 'discord' }</button>
-			</div>
+				<div className="text-container middle">
+					<h3>links</h3>
+					<a className="link" href="https://www.youtube.com/@extoplasm" target="_blank" rel="noopener noreferrer">youtube</a>
+					<br/>
+					<a className="link" href="http://github.com/extoplasm" target="_blank" rel="noopener noreferrer">github</a>
+					<br/>
+					<button className="link" onClick={() => {
+						copyToClipboard('@extoplasm')
+						setCopiedDiscord(true)
+						setTimeout(() => {
+							setCopiedDiscord(false)
+						}, 1000)
+					}}>{copiedDiscord ? 'copied to clipboard!': 'discord' }</button>
+				</div>
+			</main>
 			<Canvas className="canvas">
 				<ambientLight intensity={Math.PI / 2} />
 				<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={1} />
