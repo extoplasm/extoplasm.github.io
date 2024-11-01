88ffbb8bcf62c0ced45562aef7ab1f9e69715879
extoplasm <pleassehelsp@gmail.com>
2024-11-01 18:53:00 +1100

    feat: change on hover

--- src/App.tsx
+++ src/App.tsx
@@ -10,39 +10,47 @@ function copyToClipboard(text: string) {
         });
 }
 
-function Bio() {
+function Content() {
+	const [copiedDiscord, setCopiedDiscord] = useState(false);
+	const [hoveredLink, setHoveredLink] = useState(false);
 	return (
 		<>
-			<h1 className="margin-10">extoplasm ©</h1>
-			<h2 className="margin-10">i code (sometimes)</h2>
+			<main className="content">
+			<div className="text-container">
+				<h1 className="margin-10">extoplasm ©</h1>
+				<h2 className="margin-10">i code (sometimes)</h2>
+			</div>
+			<div className="text-container margin-10"
+				onMouseEnter={() => {
+					setHoveredLink(true)
+				}}
+				onMouseLeave={() => {
+					setHoveredLink(false)
+				}}
+			>
+				<h3>{hoveredLink ? 'links 👇' : 'links'}</h3>
+				<a className="link" href="https://www.youtube.com/@extoplasm" target="_blank" rel="noopener noreferrer">youtube</a>
+				<br/>
+				<a className="link" href="http://github.com/extoplasm" target="_blank" rel="noopener noreferrer">github</a>
+				<br/>
+				<button className="link" onClick={() => {
+					copyToClipboard('@extoplasm')
+					setCopiedDiscord(true)
+					setTimeout(() => {
+						setCopiedDiscord(false)
+					}, 1000)
+				}}>{copiedDiscord ? 'copied to clipboard!': 'discord' }</button>
+			</div>
+			</main>
 		</>
 	)
 }
 
 export default function App() {
-	const [copiedDiscord, setCopiedDiscord] = useState(false)
 	return (
 		<>
-			<main className="content">
-				<div className="text-container">
-					<Bio/>
-				</div>
-				<div className="text-container margin-10">
-					<h3>links</h3>
-					<a className="link" href="https://www.youtube.com/@extoplasm" target="_blank" rel="noopener noreferrer">youtube</a>
-					<br/>
-					<a className="link" href="http://github.com/extoplasm" target="_blank" rel="noopener noreferrer">github</a>
-					<br/>
-					<button className="link" onClick={() => {
-						copyToClipboard('@extoplasm')
-						setCopiedDiscord(true)
-						setTimeout(() => {
-							setCopiedDiscord(false)
-						}, 1000)
-					}}>{copiedDiscord ? 'copied to clipboard!': 'discord' }</button>
-				</div>
-			</main>
-			<Canvas className="canvas">
+			<Content/>
+			<Canvas className="canvas" fallback={<div>webgl not supported</div>}>
 				<ambientLight intensity={0.1} />
 				<spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={1} />
 				<Ghost position={[0, 0, 0]} />
