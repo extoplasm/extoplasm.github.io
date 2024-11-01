663e2bf808497f54affe87799996469fbbdf2169
extoplasm <pleassehelsp@gmail.com>
2024-11-01 19:53:56 +1100

    fix: i am fuming.... github doesn't support browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,59 @@
-import Main from './pages/Main.tsx';
-import Links from './pages/Links.tsx';
-import Layout from './pages/Layout.tsx'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
+import { useState } from 'react';
+import { Canvas } from '@react-three/fiber';
+import Ghost from './assets/Ghost.tsx';
+
+function copyToClipboard(text: string) {
+	navigator.clipboard.writeText(text)
+        .catch(err => {
+            console.error('Failed to copy: ', err);
+        });
+}
+
+const Links = () => {
+	const [copiedDiscord, setCopiedDiscord] = useState(false);
+	const [hoveredLink, setHoveredLink] = useState(false);
+	return (
+		<>
+			<div className="text-container margin-10"
+				onMouseEnter={() => {
+					setHoveredLink(true)
+				}}
+				onMouseLeave={() => {
+					setHoveredLink(false)
+				}}
+			>
+				<h3>{hoveredLink ? 'links 👇' : 'links'}</h3>
+				<a className="link" href="https://www.youtube.com/@extoplasm" target="_blank" rel="noopener noreferrer">youtube</a>
+				<br/>
+				<a className="link" href="http://github.com/extoplasm" target="_blank" rel="noopener noreferrer">github</a>
+				<br/>
+				<button className="link" onClick={() => {
+					copyToClipboard('@extoplasm')
+					setCopiedDiscord(true)
+					setTimeout(() => {
+						setCopiedDiscord(false)
+					}, 1000)
+				}}>{copiedDiscord ? 'copied to clipboard!': 'discord' }</button>
+			</div>
+		</>
+	)
+}
 
 export default function App() {
 	return (
 		<>
-			<BrowserRouter>
-				<Routes>
-					<Route path='/' element={<Layout/>}>
-						<Route index element={<Main />} />
-						<Route path='links' element={<Links />} />
-						<Route path='*' element={<div>no page found 404</div>} />
-					</Route>
-				</Routes>
-			</BrowserRouter>
+            <main className="content">
+                <div className="text-container">
+                    <h1 className="margin-10">extoplasm ©</h1>
+                    <h2 className="margin-10">i code (sometimes)</h2>
+                </div>
+                <Links/>
+            </main>
+            <Canvas className="canvas" fallback={<div>webgl not supported</div>}>
+                <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={1} />
+                <ambientLight intensity={0.1} />
+                <Ghost position={[0, 0, 0]} />
+            </Canvas>
 		</>
 	)
 }
